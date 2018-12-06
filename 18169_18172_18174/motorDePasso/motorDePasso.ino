a0e1f149868972d9ab6c6b46ae898cc2f79c085b
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2018-12-06 16:53:22 -0200

    c

--- 18169_18172_18174/motorDePasso/motorDePasso.ino
+++ 18169_18172_18174/motorDePasso/motorDePasso.ino
@@ -4,9 +4,9 @@ Stepper passo(500, 8, 10, 9, 11);
 void setup() {
   // put your setup code here, to run once:
   passo.setSpeed(45); //ativação do motor de passo
+  passo.step(-100);
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
-  passo.step(512);
+  
 }
--- 18169_18172_18174/sensorDeLuz/sensorDeLuz.ino
+++ 18169_18172_18174/sensorDeLuz/sensorDeLuz.ino
@@ -1,3 +1,8 @@
+int somatoria = 0;
+int maiorValor = 0;
+int menorValor = 0;
+int meuCont = 0;
+ 
  void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -5,6 +10,23 @@
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(200);
   int valorSensor = analogRead(A0);
-  Serial.println(valorSensor);
+  meuCont++;
+  Serial.print("|");
+  somatoria += valorSensor;
+  if (valorSensor > maiorValor) maiorValor = valorSensor;
+  if (valorSensor < menorValor) menorValor = valorSensor;
+  if (meuCont == 30)
+  {
+    int media = somatoria / meuCont;
+    Serial.println();
+    Serial.print("Media: ");
+    Serial.println(media);
+    Serial.print("Maior valor: ");
+    Serial.println(maiorValor);
+    Serial.print("Menor valor: ");
+    Serial.println(menorValor);
+    meuCont = 0;
+  }
 }
